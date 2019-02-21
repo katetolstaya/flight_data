db52d52e9cbf57c08c57a1410fcb1e162b8e0405
Kate Tolstaya <kate.tolstaya@gmail.com>
2019-02-21 13:40:15 -0500

    restructured load data

--- plot_obj_mult.py
+++ plot_obj_mult.py
@@ -4,11 +4,12 @@ from mpl_toolkits.mplot3d import Axes3D
 import time
 import configparser
 import random
+
 from planning.grid import Grid
 from planning.dubins_problem import DubinsProblem
-from train_mult import get_multi_airplane_segments, time_sync_flight_data
 from planning.dubins_multi_objective import DubinsMultiAirplaneObjective
-from data_utils import load_flight_data, make_planner, load_lims
+from data_utils import load_flight_data, make_planner, load_lims, get_multi_airplane_segments, time_sync_flight_data
+
 
 def main():
     config_file = 'params.cfg'
@@ -24,8 +25,8 @@ def main():
         random.seed(seed)
 
     # get plane data
-    flight_summaries = load_flight_data()
-    random.shuffle(flight_summaries)
+    fnames = ['flights20160112'] #, 'flights20160112', 'flights20160113']
+    flight_summaries = load_flight_data(fnames)
 
     print('Loading cost...')
     folder = "model/"
@@ -35,11 +36,11 @@ def main():
     obj = DubinsMultiAirplaneObjective(config, grid)
     problem = DubinsProblem(config, xyzbea_min, xyzbea_max)
 
-    max_x = 400
-    max_y = 400
+    max_x = 300
+    max_y = 300
 
-    offset_x = -200
-    offset_y = -700
+    offset_x = -250
+    offset_y = -750
 
     offset = np.array([offset_x, offset_y, 0, 0, 0]).reshape((1, 5))
     cost_min = np.ones((max_x, max_y)) * 100
@@ -69,7 +70,7 @@ def main():
 
     lists.pop(0)
     lists.pop(0)
-
+    lists.pop(0)
 
     for l in lists:
 
@@ -79,43 +80,38 @@ def main():
         paths = time_sync_flight_data(l, problem)
         obj.clear_obstacles()
 
-        #for expert_path in l:
         for expert_path in paths:
-            #expert_path = expert_path.to_path()
 
             if len(learner_trajs) >= 4:
                 break
 
             print('Planning #' + str(len(learner_trajs)+1))
 
-            expert_path_ind = problem.path_to_ind(expert_path)
+            # expert_path_ind = problem.path_to_ind(expert_path)
+            # planner_path_grid = np.zeros((expert_path_ind.shape[0], 5))
+            # for t in range(expert_path_ind.shape[0]):
+            #     ind = np.append(np.asarray(grid.ind_to_index(expert_path_ind[t, :])), expert_path_ind[t, 4])
+            #     planner_path_grid[t, :] = ind
+            # planner_path_grid = planner_path_grid + offset
+            # learner_trajs.append(planner_path_grid)
 
-            print(expert_path_ind)
-            planner_path_grid = np.zeros((expert_path_ind.shape[0], 5))
-            for t in range(expert_path_ind.shape[0]):
-                ind = np.append(np.asarray(grid.ind_to_index(expert_path_ind[t, :])), expert_path_ind[t, 4])
-                planner_path_grid[t, :] = ind
-            planner_path_grid = planner_path_grid + offset
-            learner_trajs.append(planner_path_grid)
-            #
-            # node = planner(problem, expert_path[0, :], expert_path[-1, :], obj).plan(to)
-            #
-            # if node is not None:
-            #
-            #     planner_path = problem.reconstruct_path(node)
-            #     planner_path_ind = problem.reconstruct_path_ind(node)
-            #     obj.add_obstacle(planner_path_ind)
-            #
-            #     planner_path_grid = np.zeros((planner_path_ind.shape[0], 5))
-            #     for t in range(planner_path_ind.shape[0]):
-            #         ind = np.append(np.asarray(grid.ind_to_index(planner_path_ind[t, :])), planner_path[t, 4])
-            #         planner_path_grid[t, :] = ind
-            #     planner_path_grid = planner_path_grid + offset
-            #     learner_trajs.append(planner_path_grid)
-            #
-            # else:
-            #     print('Timeout')
-            #     break
+            node = planner(problem, expert_path[0, :], expert_path[-1, :], obj).plan(to)
+
+            if node is not None:
+                planner_path = problem.reconstruct_path(node)
+                planner_path_ind = problem.reconstruct_path_ind(node)
+                obj.add_obstacle(planner_path_ind)
+
+                planner_path_grid = np.zeros((planner_path_ind.shape[0], 5))
+                for t in range(planner_path_ind.shape[0]):
+                    ind = np.append(np.asarray(grid.ind_to_index(planner_path_ind[t, :])), planner_path[t, 4])
+                    planner_path_grid[t, :] = ind
+                planner_path_grid = planner_path_grid + offset
+                learner_trajs.append(planner_path_grid)
+
+            else:
+                print('Timeout')
+                break
 
         n_learners = len(learner_trajs)
         if n_learners > 1:
@@ -125,23 +121,37 @@ def main():
             end_time = learner_trajs[-1][-1,4]
 
             lines = []
+            markers = []
             fig, ax = plt.subplots()
             ax.imshow(-1.0 * cost_min, extent=[0, max_x, 0, max_y], cmap='Greens', interpolation='spline16', alpha=0.5, origin='lower')
 
             for i in range(len(learner_trajs)):
-                line, = ax.plot([0,1], [0,1], linewidth=3, color=colors[i])
+                line, = ax.plot([0,1], [0,1], linewidth=2, color=colors[i])
                 lines.append(line)
 
+            for i in range(len(learner_trajs)):
+                marker, = ax.plot([0,1], [0,1], linewidth=0, marker='o', markersize=10, color=colors[i])
+                markers.append(marker)
+
             lines.reverse()
+            markers.reverse()
 
             inds = np.zeros((n_learners,),dtype=np.int)
 
             for t in np.arange(start_time, end_time, dt):
                 for i in reversed(range(n_learners)):
                     if learner_trajs[i].shape[0] > inds[i] and t <= learner_trajs[i][inds[i], 4]:
-                        lines[i].set_xdata(learner_trajs[i][0:inds[i], 0])
-                        lines[i].set_ydata(learner_trajs[i][0:inds[i], 1])
+                        lines[i].set_xdata(learner_trajs[i][0:inds[i]+1, 0])
+                        lines[i].set_ydata(learner_trajs[i][0:inds[i]+1, 1])
+                        markers[i].set_xdata(learner_trajs[i][inds[i], 0])
+                        markers[i].set_ydata(learner_trajs[i][inds[i], 1])
                         inds[i] = inds[i] + 1
+
+                    if learner_trajs[i][-1, 4] < t:
+                        markers[i].set_marker("None")
+                        markers[i].set_marker("None")
+
+
                 fig.canvas.draw()
                 fig.canvas.flush_events()
                 time.sleep(1.0)
--- train_mult.py
+++ train_mult.py
@@ -1,41 +1,12 @@
 import random
-from process import get_min_max_all
 import configparser
 from planning.dubins_problem import DubinsProblem
 from train import load_flight_data, make_planner, log
 from planning.dubins_multi_objective import DubinsMultiAirplaneObjective
 from planning.grid import Grid
+from data_utils import load_flight_data, make_planner, load_lims, get_multi_airplane_segments, time_sync_flight_data
 
-def get_multi_airplane_segments(flight_summaries):
-    overlap_length = 200
-    lists = []
-    for s in flight_summaries:
-        added = False
-        for l in lists:
-            for s2 in l:
-                if s.overlap(s2) > overlap_length:
-                    added = True
-                    l.append(s)
-                    break
-            if added:
-                break
-
-        if not added:
-            lists.append([s])
-
-    # remove non-overlapping trajectories
-    # and sort each list of trajectories in order of airplane's start time
-    lists = [sorted(l, key=lambda x: x.time[0]) for l in lists if len(l) >= 2]
-    return lists
-
-
-def time_sync_flight_data(flights, problem):
-    paths = []
-    s = 0.0
-    for flight in flights:
-        paths.append(problem.resample_path_dt(flight.to_path(), s, problem.dt))
-    return paths
-
+inf = float("inf")
 
 def main():
     # read in parameters
@@ -54,32 +25,19 @@ def main():
         random.seed(seed)
 
     print('Processing trajectories...')
-    flight_summaries = load_flight_data()
-    xyzbea_min, xyzbea_max = get_min_max_all(flight_summaries)
+    fnames = ['flights20160112'] #, 'flights20160112', 'flights20160113']
+    flight_summaries = load_flight_data(fnames)
     lists = get_multi_airplane_segments(flight_summaries)
     random.shuffle(lists)
 
-    print('Initializing the grid...')
-    # initialize grid with one pass through the data
-    grid = Grid(config, xyzbea_min, xyzbea_max)
-    grid.load_grid()
-    # for flight in flight_summaries:
-    #     if flight.get_path_len() < 5:  # can't interpolate paths with len < 4
-    #         continue
-    #     path = flight.to_path()
-    #     start, goal = flight.get_start_goal()
-    #     # use a dense interpolation with 200 pts
-    #     dense_path = DubinsProblem.resample_path(path, n_samples)
-    #     # grid.update(dense_path, -1000.0 * n_iters)
-    #     # grid.gradient_step(dense_path, -1000.0 * n_iters)
-    #     grid.gradient_step(dense_path, -1000.0)
-    # grid.save_grid()
-
-    # set up planner and objectives
-    problem = DubinsProblem(config, xyzbea_min, xyzbea_max)
+    folder = "model/"
+    fname = "grid19"
+    xyzbea_min, xyzbea_max = load_lims(folder, fname)
+    grid = Grid(config, xyzbea_min, xyzbea_max, fname=fname)
     obj = DubinsMultiAirplaneObjective(config, grid)
     obj_expert = DubinsMultiAirplaneObjective(config, grid)
     planner = make_planner(config['planner_type'])
+    problem = DubinsProblem(config, xyzbea_min, xyzbea_max)
 
     n_updates = 0
 
@@ -96,13 +54,14 @@ def main():
             timeout = False
 
             for expert_path in paths:
-
-                # plan trajectory
+                print(obj.obstacle_lims)
 
                 expert_path_ind = problem.path_to_ind(expert_path)
                 expert_dense_path = DubinsProblem.resample_path_dt(expert_path, s=0.1, dt=1.0)
                 expert_cost = obj_expert.integrate_path_cost(expert_path, expert_path_ind)
+
                 if not timeout:
+                    # plan trajectory
                     node = planner(problem, expert_path[0, :], expert_path[-1, :], obj).plan(to)
                     if node is not None:
 
@@ -115,28 +74,21 @@ def main():
 
                         log(str(ind) + '\t' + str(planner_cost) + '\t' + str(expert_cost) + '\t' + str(path_diff), log_file)
 
-                        ################################
                         # gradient step
-
-                        # grid.gradient_step(expert_dense_path, -1.0)
-                        # grid.gradient_step(planner_dense_path, 1.0)
-
                         obj.update_obstacle_lims(expert_path_ind, planner_path_ind, 1.0)
                         obj_expert.obstacle_lims = obj.obstacle_lims
-
-                        ###############################
 
                         # add this plane's trajectory to obstacles for the next plane
                         obj.add_obstacle(planner_path_ind)
                         obj_expert.add_obstacle(expert_path_ind)
                         n_updates = n_updates + 1
 
-                        print(obj.obstacle_lims)
                         ind = ind + 1
                     else:
                         timeout = True
 
                 if timeout:
+                    log(str(ind) + '\t' + '0' + '\t' + str(expert_cost) + '\t' + str(inf), log_file)
                     obj.update_obstacle_lims(expert_path_ind, None, 1.0)
                     obj_expert.obstacle_lims = obj.obstacle_lims
                     obj_expert.add_obstacle(expert_path_ind)

b950786b797d545229bb933c9eaa7b35ad2eb9b1
Kate Tolstaya <kate.tolstaya@gmail.com>
2019-02-25 09:53:13 -0500

    logging fixed

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ def main():
                     planner_cost = obj.integrate_path_cost(planner_dense_path)
                     path_diff = problem.compute_avg_path_diff(expert_dense_path, planner_dense_path)
 
-                    log_fname(str(ind) + '\t' + str(planner_cost) + '\t' + str(expert_cost) + '\t' + str(path_diff), log_file_name)
+                    log_fname(str(ind) + '\t' + str(planner_cost) + '\t' + str(expert_cost) + '\t' + str(path_diff), log_file_name, True)
                     # print(planner_cost - expert_cost)
                     grid.gradient_step(planner_dense_path, 10.0)
                     grid.gradient_step(expert_dense_path, -10.0)
@@ -94,7 +94,7 @@ def main():
                 expert_only = True
 
             if expert_only:
-                log_fname(str(ind) + '\t' + '0\t' + str(expert_cost) + '\t' + str(np.inf), log_file_name)
+                log_fname(str(ind) + '\t' + '0\t' + str(expert_cost) + '\t' + str(np.inf), log_file_name, True)
                 grid.gradient_step(expert_dense_path, -10.0)
 
             ind = ind + 1

c70ce9fea53afbb310c46ceb3f76005a45a22826
Kate Tolstaya <kate.tolstaya@gmail.com>
2019-03-01 12:30:10 -0500

    test avg min path diff

--- plot_objective.py
+++ plot_objective.py
@@ -17,39 +17,87 @@ planner_cost = d[:, 1]
 expert_cost = d[:, 2]
 diff = d[:, 3]
 
+diff = 1.0 / diff
+
+step_ind = 50
+min_ind = 1000
+num_inds = int(np.shape(planner_cost)[0] / step_ind)
+
+avg_diff = np.zeros((num_inds,))
+for i in range(num_inds):
+    avg_diff[i] = float(np.mean(diff[i * step_ind:((i + 1) * step_ind)]))
+
+num_inds = np.shape(avg_diff)[0]
+inds = np.linspace(0, num_inds * step_ind, num_inds)
+fig, ax = plt.subplots()
+ax.plot(inds, avg_diff)
+plt.xlabel('Training Steps')
+plt.ylabel("Reciprocal of Differences $\\sum_t \\frac{1}{|| \\bar{\\mathbf{s}}(t) - \\bar{\\mathbf{s}}^e(t)||}$")
+# plt.title("IRL Objective")
+a = gca()
+# a.set_xticklabels(a.get_xticks(), font)
+# a.set_yticklabels(a.get_yticks(), font)
+plt.tight_layout()
+plt.show()
+
+# diff[diff == np.inf] = np.nan
+# avg_diff = np.zeros((num_inds,))
+# for i in range(num_inds):
+#     avg_diff[i] = np.nanmean(diff[i * step_ind:((i + 1) * step_ind)])
+#
+# num_inds = np.shape(avg_diff)[0]
+# inds = np.linspace(0, num_inds * step_ind, num_inds)
+# fig, ax = plt.subplots()
+# ax.plot(inds, avg_diff)
+# plt.xlabel('Training Steps')
+# plt.ylabel("Distance Between Expert and Learner")
+# # plt.title("IRL Objective")
+# a = gca()
+# a.set_xticklabels(a.get_xticks(), font)
+# a.set_yticklabels(a.get_yticks(), font)
+# plt.tight_layout()
+# plt.show()
+
+step_ind = 50
+min_ind = 1000
+num_inds = int(np.shape(planner_cost)[0] / step_ind)
+
 obj = planner_cost - expert_cost
 
 N = 50
 
+##################################################################################################
 ### Objective Plot
-filt_obj = np.convolve(obj, np.ones((N,))/N, mode='same')
+filt_obj = np.convolve(obj, np.ones((N,)) / N, mode='same')
+
+avg_cost = np.zeros((num_inds,))
+for i in range(num_inds):
+    avg_cost[i] = np.sum(obj[i * step_ind:((i + 1) * step_ind)]) / step_ind
+
+num_inds = np.shape(avg_cost)[0]
+inds = np.linspace(0, num_inds * step_ind, num_inds)
+
 fig, ax = plt.subplots()
-ax.plot(inds, filt_obj)
+ax.plot(inds, avg_cost)
 plt.xlabel('Training Steps')
-plt.ylabel("Margin $\hat{\mathcal{L}}(J_a)$") # plt.ylabel("Objective $\mathcal{L}(J_a)$")
-#plt.title("IRL Objective")
+plt.ylabel("Margin $\hat{\mathcal{L}}(J_a)$")  # plt.ylabel("Objective $\mathcal{L}(J_a)$")
+# plt.title("IRL Objective")
 a = gca()
 a.set_xticklabels(a.get_xticks(), font)
 a.set_yticklabels(a.get_yticks(), font)
 plt.tight_layout()
 plt.show()
 
-
+################################################################################################
 ### % Timeout plot
-planner_cost_nonzero = planner_cost > 0
-step_ind = 50
-min_ind = 1000
-num_inds = int(np.shape(planner_cost)[0] / step_ind)
 freq_timeout = np.zeros((num_inds,))
+planner_cost_nonzero = planner_cost > 0
 
 for i in range(num_inds):
     freq_timeout[i] = np.sum(planner_cost_nonzero[i * step_ind:((i + 1) * step_ind)])
-
 freq_timeout[int(min_ind / step_ind):] = freq_timeout[int(min_ind / step_ind):] / step_ind
 
-N = 1
-freq_timeout = np.convolve(freq_timeout, np.ones((N,)) / N, mode='valid')
-num_inds = np.shape(freq_timeout)[0]
+num_inds = np.shape(avg_cost)[0]
 inds = np.linspace(0, num_inds * step_ind, num_inds)
 
 fig, ax = plt.subplots()
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from planning.grid import Grid
 from planning.dubins_objective import DubinsObjective
 
 from planning.dubins_problem import DubinsProblem
-from data_utils import load_flight_data, make_planner, load_lims
+from data_utils import load_flight_data, make_planner, load_lims, log
 from matplotlib import rc
 
 rc('text', usetex=True)
@@ -38,13 +38,14 @@ def main():
 
     print('Loading cost...')
     folder = "model/"
-    fname = "grid19"
+    fname = "grid22"
     xyzbea_min, xyzbea_max = load_lims(folder, fname)
     grid = Grid(config, xyzbea_min, xyzbea_max, fname=fname)
     obj = DubinsObjective(config, grid)
     problem = DubinsProblem(config, xyzbea_min, xyzbea_max)
 
     print('Planning...')
+    ind = 0
     for flight in flight_summaries:
         if flight.get_num_waypoints() < 4:
             continue
@@ -54,14 +55,18 @@ def main():
             planner_path = problem.reconstruct_path(node)
 
             expert_path = flight.to_path()
-            planner_spline = problem.resample_path(planner_path, n_samples)
+            #planner_spline = problem.resample_path(planner_path, n_samples)
             expert_spline = problem.resample_path(expert_path, n_samples)
             expert_cost = obj.integrate_path_cost(expert_path)
             planner_cost = obj.integrate_path_cost(planner_path)
-            print(str(planner_cost - expert_cost))
+            #print(str(planner_cost - expert_cost))
 
-            plot_planner_expert(planner_path, expert_path, planner_spline, expert_spline)
+            path_min_diff = problem.compute_avg_min_diff(planner_path, expert_spline)
 
+            log(str(ind)+ '\t' + str(planner_cost) + '\t' + str(expert_cost) + '\t' + str(path_min_diff))
+
+            #plot_planner_expert(planner_path, expert_path, planner_spline, expert_spline)
+            ind = ind + 1
         else:
             print('Timeout')
 
